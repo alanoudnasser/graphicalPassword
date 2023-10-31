d67f2e6c46e96c257af4ba5abcf871dd69cad14e
Sarahkhj <xladysarah@gmail.com>
2023-10-31 21:52:07 +0300

    update weelView

--- app/src/main/java/com/example/graphpassword/WheelView.java
+++ app/src/main/java/com/example/graphpassword/WheelView.java
@@ -8,9 +8,14 @@ import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+
 
 public class WheelView extends View {
     private static final int NUM_SECTORS = 8;
@@ -32,6 +37,7 @@ public class WheelView extends View {
     private Random random;
     private RectF sectorRect;
     private float rotationAngle;
+    private int selectedSector = -1;
 
     public WheelView(Context context) {
         super(context);
@@ -64,7 +70,15 @@ public class WheelView extends View {
 
         random = new Random();
         sectorRect = new RectF();
+        shuffleAlphabets();
+
     }
+    private void shuffleAlphabets() {
+        List<String> alphabetList = new ArrayList<>(Arrays.asList(ALPHABETS));
+        Collections.shuffle(alphabetList);
+        alphabetList.toArray(ALPHABETS);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -79,12 +93,9 @@ public class WheelView extends View {
         int startAngle = 0;
         int sweepAngle = 360 / NUM_SECTORS;
 
-        edgePaint.setStrokeWidth(10);  // Adjust the stroke width to make the edges thicker
-
         for (int i = 0; i < NUM_SECTORS; i++) {
             int endAngle = startAngle + sweepAngle;
 
-            // Draw the sector edge with different colors
             edgePaint.setColor(SECTOR_COLORS[i % SECTOR_COLORS.length]);
 
             sectorRect.set(
@@ -94,30 +105,26 @@ public class WheelView extends View {
                     height / 2 + radius
             );
 
-            // Draw the sector edge without filling
             canvas.drawArc(sectorRect, startAngle, sweepAngle, false, edgePaint);
 
-            // Draw black lines to divide the sector
             linePaint.setColor(Color.BLACK);
-            linePaint.setStrokeWidth(2);  // Adjust the stroke width for the lines
+            linePaint.setStrokeWidth(2);
 
             Path linePath = new Path();
             linePath.arcTo(sectorRect, startAngle, sweepAngle);
             canvas.drawPath(linePath, linePaint);
 
-            // Draw the alphabet in the center of the sector with rotation
             float centerX = width / 2f;
             float centerY = height / 2f;
             float angle = (startAngle + endAngle) / 2f;
             float x = centerX + (float) (radius * 0.75 * Math.cos(Math.toRadians(angle)));
             float y = centerY + (float) (radius * 0.75 * Math.sin(Math.toRadians(angle)));
 
-            canvas.save();  // Save the canvas state
-            canvas.rotate(rotationAngle, centerX, centerY);  // Apply rotation transformation
+            canvas.save();
+            canvas.rotate(rotationAngle, centerX, centerY);
             canvas.drawText(ALPHABETS[i], x, y, textPaint);
-            canvas.restore();  // Restore the canvas state
+            canvas.restore();
 
-            // Draw lines to divide the sector
             float lineStartX = width / 2f;
             float lineStartY = height / 2f;
             float lineEndX = (float) (lineStartX + radius * Math.cos(Math.toRadians(startAngle)));
@@ -125,12 +132,10 @@ public class WheelView extends View {
 
             canvas.drawLine(lineStartX, lineStartY, lineEndX, lineEndY, linePaint);
 
-            // Update the start angle for the next sector
             startAngle = endAngle;
         }
 
-        // Draw an outer stroke for the circle with different colors
-        edgePaint.setStrokeWidth(5);  // Adjust the stroke width as needed
+        edgePaint.setStrokeWidth(5);
         edgePaint.setStyle(Paint.Style.STROKE);
 
         for (int i = 0; i < NUM_SECTORS; i++) {
@@ -138,13 +143,89 @@ public class WheelView extends View {
             canvas.drawArc(sectorRect, i * sweepAngle, sweepAngle, false, edgePaint);
         }
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                int newlySelectedSector = getTouchedSector(x, y);
+                if (newlySelectedSector != -1) {
+                    String selectedLetter = ALPHABETS[newlySelectedSector];
+                    int colorIndex = newlySelectedSector % SECTOR_COLORS.length;
+                    int sectorColor = SECTOR_COLORS[colorIndex];
+                    String colorName = getColorName(sectorColor);
+                    Toast.makeText(getContext(), "Selected letter: " + selectedLetter + ", Color: " + colorName, Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+
+
+
+    private String getColorName(int color) {
+        if (color == Color.BLUE) {
+            return "Blue";
+        } else if (color == Color.GREEN) {
+            return "Green";
+        } else if (color == Color.YELLOW) {
+            return "Yellow";
+        } else if (color == Color.MAGENTA) {
+            return "Magenta";
+        } else if (color == Color.CYAN) {
+            return "Cyan";
+        } else if (color == Color.RED) {
+            return "Red";
+        } else if (color == Color.GRAY) {
+            return "Gray";
+        } else if (color == Color.DKGRAY) {
+            return "Dark Gray";
+        } else {
+            return "Unknown";
+        }
+    }
+
+    private int getTouchedSector(float x, float y) {
+        int width = getWidth();
+        int height = getHeight();
+        int radius = Math.min(width, height) / 2;
+
+        float centerX = width / 2f;
+        float centerY = height / 2f;
+
+        // Calculate the angle based on the touch coordinates
+        double angle = Math.toDegrees(Math.atan2(y - centerY, x - centerX));
+        angle = (angle < 0) ? (360 + angle) : angle;
+
+        // Adjust for the current rotation angle
+        angle -= rotationAngle;
+        if (angle < 0) {
+            angle += 360;
+        }
+
+        // Compensate for the selected sector based on the current angle and rotation
+        int sector = (int) (angle * NUM_SECTORS / 360);
+
+        return sector;
+    }
+
+
+
+
+
     public void rotateClockwise() {
-        rotationAngle += 10;
+        rotationAngle += 45;
         invalidate();
     }
 
     public void rotateCounterClockwise() {
-        rotationAngle -= 10;
+        rotationAngle -= 45;
         invalidate();
     }
 }
